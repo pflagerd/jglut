8f9de186dfcea52f069a42064ae1a8f1a69f5a9d
Daniel P Pflager <daniel.pflager@gmail.com>
2022-01-01 21:21:44 -0800

    Make the template so it will compile so it can be included in a $(wildcard    ) call.

--- src/Java_com_pflager_template.c
+++ src/Java_com_pflager_template.c
@@ -5,7 +5,7 @@
 
 #include "com_pflager_gl.h"
 
-PASTE_SIGNATURE_HERE {
+void PASTE_SIGNATURE_HERE(JNIEnv *env, jobject object) {
 	DPRINTF("glutSomething()\n");
 
     jclass notImplementedException = (*env)->FindClass(env, "com/pflager/glut$NotImplementedException");
